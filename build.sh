37daae5a60c99118e015c505c09fb24313f489f6
Charlie Chen <charlie@findora.org>
2022-11-15 18:33:16 -0600

    first commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd src/circuits
+
+# zkcredit_650
+./build_circuit.sh zkcredit zkcredit_650 powersOfTau28_hez_final_17.ptau
+
+# zkcredit_700
+./build_circuit.sh zkcredit zkcredit_700 powersOfTau28_hez_final_17.ptau
