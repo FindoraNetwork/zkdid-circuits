eb2267074b973e61db3e0c72bbdd24eaa92799fc
Charlie Chen <charlie@findora.org>
2022-11-17 17:43:12 -0600

    added zkcredit_v2 to check dob

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ cd src/circuits
 
 # zkcredit_700
 ./build_circuit.sh zkcredit zkcredit_700 powersOfTau28_hez_final_17.ptau
+
+# zkcredit_v2_700_2004
+./build_circuit.sh zkcredit_v2 zkcredit_v2_700_2004 powersOfTau28_hez_final_17.ptau
